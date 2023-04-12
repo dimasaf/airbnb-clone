adc96b9f53836c99afed47b8f4acf47fe5a8a3c8
dimasaf <dimas.adji@ifg-life.id>
2023-04-12 20:09:15 +0700

    init and fix signup modal

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useState, useCallback, useEffect } from "react";
+import { IoMdClose } from "react-icons/io";
+
+import Button from "../Button";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  actionLabel: string;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  disabled?: boolean;
+  secondaryLabel?: string;
+  secondaryAction?: () => void;
+}
+
+const Modal = (props: ModalProps) => {
+  const {
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryLabel,
+    secondaryAction,
+  } = props;
+
+  const [showModal, setShowModal] = useState(isOpen);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    setShowModal(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen, onClose]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  console.warn(showModal);
+
+  return (
+    <div className="bg-neutral-800/70 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+        {/* content */}
+        <div
+          className={`translate duration-300 h-full ${
+            showModal
+              ? "translate-y-0 opacity-100"
+              : "translate-y-full opacity-0"
+          }`}
+        >
+          <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/* HEADER */}
+            <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+              <button
+                className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                onClick={handleClose}
+              >
+                <IoMdClose size={18} />
+              </button>
+              <div className="text-lg font-semibold">{title}</div>
+            </div>
+
+            {/* BODY */}
+            <div className="relative p-6 flex-auto">{body}</div>
+
+            {/* FOOTER */}
+            <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-row items-center gap-4 w-full">
+                {secondaryAction && secondaryLabel && (
+                  <Button
+                    label={secondaryLabel}
+                    onClick={handleSecondaryAction}
+                    disabled={disabled}
+                    outline
+                  />
+                )}
+                <Button
+                  label={actionLabel}
+                  onClick={handleSubmit}
+                  disabled={disabled}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      title="Sign Up"
+      actionLabel="Continue"
+    />
+  );
+};
+
+export default RegisterModal;
