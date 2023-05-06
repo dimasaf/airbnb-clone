0fba70fff215ad7a2e77b458a13fd6ba7306cd64
dimasaf <dimas.adji@ifg-life.id>
2023-05-06 19:49:16 +0700

    feat: finish steps listing and api

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useState, useCallback, useMemo } from "react";
@@ -18,6 +19,8 @@ import Button from "../Button";
 
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
+import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 import { categoriesDummy } from "../navbar/Categories";
 import dynamic from "next/dynamic";
@@ -61,6 +64,10 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -84,6 +91,30 @@ const RentModal = () => {
 
   const onNext = () => {
     setStep((val) => val + 1);
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post("/api/listings", data)
+      .then(() => {
+        toast.success("List Created!");
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch(() => {
+        toast.error("Something went wrong.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   const actionLabel = useMemo(() => {
@@ -148,6 +179,94 @@ const RentModal = () => {
             <Map center={location?.latlng} />
           </div>
         );
+      case STEPS.INFO:
+        return (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Share some basics about your place"
+              subtitle="What amenitis do you have?"
+            />
+            <Counter
+              onChange={(value) => setCustomValue("guestCount", value)}
+              value={guestCount}
+              title="Guests"
+              subtitle="How many guests do you allow?"
+            />
+            <hr />
+            <Counter
+              onChange={(value) => setCustomValue("roomCount", value)}
+              value={roomCount}
+              title="Rooms"
+              subtitle="How many rooms do you have?"
+            />
+            <hr />
+            <Counter
+              onChange={(value) => setCustomValue("bathroomCount", value)}
+              value={bathroomCount}
+              title="Bathrooms"
+              subtitle="How many bathrooms do you have?"
+            />
+          </div>
+        );
+      case STEPS.IMAGES:
+        return (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Add a photo of your place"
+              subtitle="Show guests what your place looks like!"
+            />
+            <ImageUpload
+              onChange={(value) => setCustomValue("imageSrc", value)}
+              value={imageSrc}
+            />
+          </div>
+        );
+      case STEPS.DESCRIPTION:
+        return (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="How would you describe your place?"
+              subtitle="Short and sweet works best!"
+            />
+            <Input
+              id="title"
+              label="Title"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+            <hr />
+            <Input
+              id="description"
+              label="Description"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        );
+      case STEPS.PRICE:
+        return (
+          <div className="flex flex-col gap-8">
+            <Heading
+              title="Now, set your price"
+              subtitle="How much do you charge per night?"
+            />
+            <Input
+              id="price"
+              label="Price"
+              formatPrice
+              type="number"
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        );
+
       default:
         break;
     }
@@ -158,7 +277,7 @@ const RentModal = () => {
       disabled={isLoading}
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       title="AirBnb Your Home"
       actionLabel={actionLabel}
       secondaryLabel={secondaryActionLabel}
